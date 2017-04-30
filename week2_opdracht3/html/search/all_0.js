f6680d5082c600f29837895ba2b9900aa3b18f71
RikHU <rik.ruttenberg@student.hu.nl>
2017-04-30 18:52:05 +0200

    added doxygen in week 2

---
+++ week2_opdracht3/html/search/all_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['operator_2a',['operator*',['../classvector.html#ad4e924ee21af1367f8724b2d94db110e',1,'vector::operator*(const int rhs) const'],['../classvector.html#a9ef97fde561d0998f1db1af7c9fbbff8',1,'vector::operator*(const vector &amp;rhs) const']]],
+  ['operator_2a_3d',['operator*=',['../classvector.html#ab719e6dfed904db0c0da869e71bd6580',1,'vector']]],
+  ['operator_2b',['operator+',['../classvector.html#a9d639bc53d77f17c6ba3af1dd9549424',1,'vector']]],
+  ['operator_2b_3d',['operator+=',['../classvector.html#acf4c6a4d343c92e211f80de7712a3cac',1,'vector']]],
+  ['operator_3c_3c',['operator&lt;&lt;',['../classvector.html#afd9185b99000e1fcd09b435089cd1505',1,'vector']]],
+  ['operator_3d_3d',['operator==',['../classvector.html#a7a25ae23c15974286951c24822c0b7ff',1,'vector']]]
+];
---
+++ week2_opdracht3/html/search/related_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['operator_3c_3c',['operator&lt;&lt;',['../classvector.html#afd9185b99000e1fcd09b435089cd1505',1,'vector']]]
+];
